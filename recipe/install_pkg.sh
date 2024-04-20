504ce647ab24795cb80bc0f33eb37fac1a5c28ce
Isuru Fernando <isuruf@gmail.com>
2024-04-20 11:26:49 -0500

    noarch: generic

--- recipe/install_pkg.sh
+++ recipe/install_pkg.sh
@@ -1,2 +1,2 @@
-mkdir -p $PREFIX/Library/mingw-w64
-cp -R $SRC_DIR/binary-${PKG_NAME}/ucrt64/* $PREFIX/Library/mingw-w64
+mkdir -p $PREFIX/x86_64-w64-mingw32/sysroot/usr
+cp -R $SRC_DIR/binary-${PKG_NAME}/ucrt64/* $PREFIX/x86_64-w64-mingw32/sysroot/usr/
