f51979660dcb97609a9492374e87b7eb5324f243
Isuru Fernando <isuruf@gmail.com>
2024-05-07 16:31:50 -0500

    use mingw in folder name

--- recipe/install_pkg.bat
+++ recipe/install_pkg.bat
@@ -1,2 +1,2 @@
-mkdir %PREFIX%\Library\mingw-w64
-xcopy %SRC_DIR%\binary-%PKG_NAME%\ucrt64 %LIBRARY_PREFIX%\mingw-w64 /s /e /y
+mkdir %PREFIX%\Library\ucrt64
+xcopy %SRC_DIR%\binary-%PKG_NAME%\ucrt64 %LIBRARY_PREFIX%\ucrt64 /s /e /y
--- recipe/install_pkg.sh
+++ recipe/install_pkg.sh
@@ -1,2 +1,2 @@
-mkdir -p $PREFIX/x86_64-w64-mingw32/sysroot/usr
-cp -R $SRC_DIR/binary-${PKG_NAME}/ucrt64/* $PREFIX/x86_64-w64-mingw32/sysroot/usr/
+mkdir -p $PREFIX/x86_64-w64-mingw32/sysroot/ucrt64
+cp -R $SRC_DIR/binary-${PKG_NAME}/ucrt64/* $PREFIX/x86_64-w64-mingw32/sysroot/ucrt64/
